78a6d124ef4a00bdc51f13c6743a0188e955f0d4
Nazarii <nazarii.spikhalskyi@gmail.com>
2022-04-10 11:58:20 +0300

    update_format

--- lib/elixir_rabbitmq/dynamic_consumer.ex
+++ lib/elixir_rabbitmq/dynamic_consumer.ex
@@ -1,51 +1,56 @@
 defmodule ElixirRabbitmq.DynamicConsumer do
-    use DynamicSupervisor
-    require Logger
+  use DynamicSupervisor
+  require Logger
 
-    def start_link(init_arg) do
-        DynamicSupervisor.start_link(__MODULE__, init_arg, name: __MODULE__)
+  def start_link(init_arg) do
+    DynamicSupervisor.start_link(__MODULE__, init_arg, name: __MODULE__)
+  end
+
+  @impl true
+  def init(init_arg) do
+    {:ok, _} =
+      Task.start_link(fn ->
+        spawn_consumers()
+      end)
+
+    DynamicSupervisor.init(
+      strategy: :one_for_one,
+      extra_arguments: [init_arg]
+    )
+  end
+
+  def id(%{id: id, name: name}) do
+    String.to_atom("consumer_#{name}_#{id}")
+  end
+
+  def stop_child(pid) do
+    Logger.info("#{__MODULE__} | Process #{inspect(pid)} successful stoped", ansi_color: :green)
+    DynamicSupervisor.terminate_child(__MODULE__, pid)
+  end
+
+  def new_child(params) do
+    module = params.consumer
+    spec = module.child_spec(params)
+
+    case DynamicSupervisor.start_child(__MODULE__, spec) do
+      {:ok, pid} -> {:ok, pid}
+      {:error, {:already_started, pid}} -> {:already_started, pid}
+      error -> raise "#{__MODULE__} new_child/1 ERROR -> #{inspect(error)}"
     end
 
-    @impl true
-    def init(init_arg) do
-        {:ok, _} = Task.start_link(fn ->
-            spawn_consumers()
-        end)
-        DynamicSupervisor.init(
-            strategy: :one_for_one,
-            extra_arguments: [init_arg]
-        )
-    end
+    DynamicSupervisor.count_children(__MODULE__) |> IO.inspect()
+  end
 
-    def id(%{id: id, name: name}) do
-        String.to_atom("consumer_#{name}_#{id}")
-    end
+  def spawn_consumers do
+    elixir_rabbitmq_queues =
+      Application.get_env(:elixir_rabbitmq, :queue_conf)[:elixir_rabbitmq].queues
 
-    def stop_child(pid) do
-        Logger.info("#{__MODULE__} | Process #{inspect(pid)} successful stoped", ansi_color: :green)
-        DynamicSupervisor.terminate_child(__MODULE__, pid)
-    end
+    queues = elixir_rabbitmq_queues
 
-    def new_child(params) do
-        module = params.consumer
-        spec = module.child_spec(params)
-
-        case DynamicSupervisor.start_child(__MODULE__, spec) do
-            {:ok, pid} -> {:ok, pid}
-            {:error, {:already_started, pid}} -> {:already_started, pid}
-            error -> raise "#{__MODULE__} new_child/1 ERROR -> #{inspect error}"
-        end
-        DynamicSupervisor.count_children(__MODULE__) |> IO.inspect()
-    end
-
-    def spawn_consumers do
-        elixir_rabbitmq_queues = Application.get_env(:elixir_rabbitmq, :queue_conf)[:elixir_rabbitmq].queues
-        queues = elixir_rabbitmq_queues
-
-        Enum.each(queues, fn queue_conf -> 
-            Enum.each(1..queue_conf.consumers_count, fn consumer_count ->
-                spawn(fn -> new_child(Map.put(queue_conf, :id, consumer_count)) end)
-            end)
-        end)
-    end
+    Enum.each(queues, fn queue_conf ->
+      Enum.each(1..queue_conf.consumers_count, fn consumer_count ->
+        spawn(fn -> new_child(Map.put(queue_conf, :id, consumer_count)) end)
+      end)
+    end)
+  end
 end
--- lib/elixir_rabbitmq/publisher.ex
+++ lib/elixir_rabbitmq/publisher.ex
@@ -1,45 +1,48 @@
 defmodule ElixirRabbitmq.Publisher do
-    use GenServer
-    require Logger
-    use AMQP
+  use GenServer
+  require Logger
+  use AMQP
 
-    def start_link(_) do
-        GenServer.start_link(__MODULE__, [], name: __MODULE__)
-    end
+  def start_link(_) do
+    GenServer.start_link(__MODULE__, [], name: __MODULE__)
+  end
 
-    def init(_) do
-        {:ok, []}
-    end
-    
-    def publish({_conn_type, _queue, _message} = opts) do
-        GenServer.cast(__MODULE__, opts)
-    end
-    
-    def handle_cast({conn_type, queue, message}, state) do
-        connection_module = Application.get_env(:elixir_rabbitmq, :queue_conf)[conn_type].connection_module
+  def init(_) do
+    {:ok, []}
+  end
 
-        case connection_module.get_channel do
-            {:ok, chan} ->
-                publish_data(chan, %{queue: queue, message: message})
-                {:noreply, state}
-            {:error, :not_connected} -> 
-                {:noreply, state}
-        end
-    end
+  def publish({_conn_type, _queue, _message} = opts) do
+    GenServer.cast(__MODULE__, opts)
+  end
 
-    defp publish_data(chan, %{queue: queue, message: message}) do
-        if Process.alive?(chan.pid) do
-            case Basic.publish(chan, "", queue, "#{message}") do
-                :ok ->
-                    Logger.info("Message #{message} succeful published", ansi_color: :blue)
-                    :ok
-                _ ->
-                    Logger.error("Failure message publish #{message}")
-                    :error
-            end
-        else
-            Logger.error("Channel #{inspect(chan)} is not alive")
-            :error
-        end
+  def handle_cast({conn_type, queue, message}, state) do
+    connection_module =
+      Application.get_env(:elixir_rabbitmq, :queue_conf)[conn_type].connection_module
+
+    case connection_module.get_channel do
+      {:ok, chan} ->
+        publish_data(chan, %{queue: queue, message: message})
+        {:noreply, state}
+
+      {:error, :not_connected} ->
+        {:noreply, state}
     end
+  end
+
+  defp publish_data(chan, %{queue: queue, message: message}) do
+    if Process.alive?(chan.pid) do
+      case Basic.publish(chan, "", queue, "#{message}") do
+        :ok ->
+          Logger.info("Message #{message} succeful published", ansi_color: :blue)
+          :ok
+
+        _ ->
+          Logger.error("Failure message publish #{message}")
+          :error
+      end
+    else
+      Logger.error("Channel #{inspect(chan)} is not alive")
+      :error
+    end
+  end
 end
